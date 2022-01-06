0ef60cf88aa7c4e475040c2741059dec2df8a3ad
Rashi Israni <rashiisrani97@gmail.com>
2022-01-07 05:26:53 +0530

    Assessment 1 block 2

---
+++ block-BJaafv/code/script.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+    return a + b;
+  }
+  
+
+let userName = 'John';
+
+function showMessage() {
+  let message = 'Hello, ' + userName;
+  return message;
+}
+
+alert(userName);
+showMessage();
+
